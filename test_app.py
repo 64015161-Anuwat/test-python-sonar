2f95166f7fc20b949b1bb5d15ff1b0fa146c31a7
64015161-Anuwat <64015161@kmitl.ac.th>
2023-09-07 14:59:36 +0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,3 +1,7 @@
 def reverse_words(s):
     string = str(s)
     return ' '.join([word[::-1] for word in string.split(' ')])
+
+def test_reverse_words():
+    assert reverse_words("Let's take LeetCode contest") == "s'teL ekat edoCteeL tsetnoc"
+    assert reverse_words("God Ding") == "doG gniD"
